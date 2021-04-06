5b7f0fab404a2e334e9ce5acaa6cff53ff09a012
UĞUR PEKTAŞ <76955236+ugurpektas@users.noreply.github.com>
2021-04-07 01:18:31 +0300

    hesap makinesi tamamlandı

--- script.js
+++ script.js
@@ -1,55 +1,97 @@
 const display = document.querySelector('.calculator-input');
 const keys = document.querySelector('.calculator-keys');
 
-let displayValue='0';
+let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 updateDisplay();
 
-function updateDisplay(){
+function updateDisplay() {
     display.value = displayValue;
 }
 
-//matches methodu kontrol etmek için kullanılır "if(!e.target.matches('button'))  tıklanılan eleman button değilse;"
-// return =>şarttaki olay gerçekleşirse alt satırdaki kodları çalıştırmaz.
-keys.addEventListener('click',function(e){
+keys.addEventListener('click', function(e) {
     const element = e.target;
+    const value = element.value;
 
-    if(!element.matches('button')) return;  // return => eğer tıkladığımız alan buton değilse fonksiyon dönmeyecek
+    if (!element.matches('button')) return;
 
-    if(element.classList.contains('oparator')) {
-        console.log('oparator',element.value);
-        return; // olay gerçekleşirse alt satırdaki kodları çalıştırmaz.
+    switch(value){
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal();
+            break;
+        case 'clear':
+            clear();
+            break;
+        default : 
+            inputNumber(value);
+            updateDisplay();
     }
-
-    if(element.classList.contains('decimal')) {
-        //console.log('decimal',element.value);
-        inputDecimal();
-        return; // olay gerçekleşirse alt satırdaki kodları çalıştırmaz.
-    }
-
-    if(element.classList.contains('clear')) {
-        //console.log('clear',element.value);
-        clear();
-        updateDisplay();
-        return; // olay gerçekleşirse alt satırdaki kodları çalıştırmaz.
-    }
-    //console.log('number',element.value);
-    inputNumber(element.value);
     updateDisplay();
 });
 
-//displayValue 0 a eşit değilse , girilen rakamlar yanına eklenerek devam edicek
-function inputNumber(num){
-    displayValue = displayValue === '0'? num: displayValue + num;
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
+
+    if(operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator) {
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
 
-//(!displayValue.includes('.')) => "includes" içermek anlamına gelir
-function inputDecimal(){
-    if (!displayValue.includes('.')){ // '.' operatorunu içermiyorsa dönecek olan döngü
+function calculate(first, second, operator) {
+    if(operator === '+') {
+        return first + second;
+    } else if (operator === '-') {
+        return first - second;
+    } else if (operator === '*') {
+        return first * second
+    } else if (operator === '/') {
+        return first / second;
+    }
+    return second;
+}
+
+function inputNumber(num) {
+    if(waitingForSecondValue) {
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+        displayValue = displayValue === '0'? num: displayValue + num;
+    }
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+}
+
+function inputDecimal() {
+    if (!displayValue.includes('.')) {
         displayValue += '.';
     }
 }
 
-function clear(){
+function clear() {
     displayValue = '0';
 }
